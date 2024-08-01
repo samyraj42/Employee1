9c21cc81c86b6418154555b2f51043add479a201
samyraj42 <45854906+samyraj42@users.noreply.github.com>
2024-08-01 14:38:06 +0530

    Add files via upload

--- WpfApp1/Controls/ImageButton.cs
+++ WpfApp1/Controls/ImageButton.cs
@@ -1,1 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
 
+namespace Controls
+{
+    public class ImageButton : Button
+    {
+        bool valuesChangedBeforeLoaded = false;
+        Image imgBlock = null;
+       
+        public ImageButton()
+        {
+            this.Loaded += ButtonWithTextAndImage_Loaded;
+            this.IsVisibleChanged += BaseButtonWithTextAndImage_IsVisibleChanged;
+        }
+
+        private void BaseButtonWithTextAndImage_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void ButtonWithTextAndImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (valuesChangedBeforeLoaded)
+            {
+                Update();
+            }
+        }
+
+        void Update()
+        {
+           
+            if (null != this.NormalIcon)
+                this.UpdateImage(this.NormalIcon);
+
+        }
+
+        /// <summary>
+        /// Tempate objs can only be get after loading template
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            imgBlock = (Image)this.Template.FindName("PART_btnImage", this);
+         
+            if (valuesChangedBeforeLoaded)
+            {
+                Update();
+            }
+        }
+
+        
+        public static readonly DependencyProperty NormalIconProperty = DependencyProperty.Register("NormalIcon", typeof(ImageSource), typeof(ImageButton), new PropertyMetadata(null, new PropertyChangedCallback(OnNormalIconChanged)));
+        public ImageSource NormalIcon
+        {
+            get { return (ImageSource)GetValue(NormalIconProperty); }
+            set { SetValue(NormalIconProperty, value); }
+        }
+        private static void OnNormalIconChanged(DependencyObject d, DependencyPropertyChangedEventArgs args)
+        {
+            ImageButton btn = d as ImageButton;
+            btn.valuesChangedBeforeLoaded = true;
+            btn.UpdateImage((ImageSource)args.NewValue);
+        }
+
+        private void UpdateImage(ImageSource newValue)
+        {
+            if (this.IsLoaded && null != imgBlock)
+            {
+                imgBlock.Source = newValue;
+
+            }
+        }
+    }
+}
---
+++ WpfApp1/Controls/SearchBox.xaml.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controls
+{
+    /// <summary>
+    /// Interaction logic for Search.xaml
+    /// </summary>
+    public partial class SearchBox : ContentControl
+    {
+        public event EventHandler RefreshEvent;
+      
+        public SearchBox()
+        {
+            InitializeComponent();
+
+        }
+        public EventHandler eventHandler
+        {
+            get { return (EventHandler)GetValue(eventHandlerProperty); }
+            set { SetValue(eventHandlerProperty, value); }
+        }
+        public static readonly DependencyProperty eventHandlerProperty = DependencyProperty.Register("eventHandler", typeof(EventHandler), typeof(SearchBox));
+
+        public void Refresh()
+        {
+            InvokeRefresh(EventArgs.Empty);
+        }
+
+        private void InvokeRefresh(EventArgs e)
+        {
+            var handler = RefreshEvent;
+            if (handler != null)
+                handler(txtSearch.Text.Trim(), e);
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RefreshEvent += eventHandler;
+            Refresh();
+        }
+    }
+}
