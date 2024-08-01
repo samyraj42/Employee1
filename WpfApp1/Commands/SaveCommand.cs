0422d6b36a1a6938b3fda49031b3940ab566e731
samyraj42 <45854906+samyraj42@users.noreply.github.com>
2024-08-01 14:35:19 +0530

    Add files via upload

---
+++ WpfApp1/Commands/SaveCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp1.Commands
+{
+    public class SaveCommand : ICommand
+    {
+        public Action<object> _excute { get; set; }
+        public Predicate<object> _canexcute { get; set; }
+
+        public SaveCommand(Action<object> ExcuteMethod,Predicate<object> CanExcuteMethod)
+        {
+            _excute = ExcuteMethod;
+            _canexcute = CanExcuteMethod;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canexcute.Invoke(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _excute.Invoke(parameter);
+        }
+    }
+}
