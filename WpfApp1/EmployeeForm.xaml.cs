149737b99fb0bdc87833d2bf0748abe77c0692b8
samyraj42 <45854906+samyraj42@users.noreply.github.com>
2024-08-01 12:14:05 +0530

    Add files via upload

---
+++ WpfApp1/EmployeeForm.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Model;
+using WpfApp1.NewFolder;
+using WpfApp1.View;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class EmployeeForm : Window
+    {
+        public EmployeeForm()
+        {
+            InitializeComponent();
+
+            EmployeeViewModel viewModel = new EmployeeViewModel();    
+            this.DataContext = viewModel;
+            txtSearch.eventHandler += StudentSearchFiler;
+        }
+
+        private void imgBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var selecteditem = dgEmployees.SelectedItem as Employee;
+            Window parentWindow = Window.GetWindow((DependencyObject)this);
+
+            var vm = ((this.DataContext) as EmployeeViewModel);
+            vm.Editstudent(selecteditem);
+            vm.Title = "Update Student";
+            vm.TAG = "U";
+
+            Window window = new Window
+            {
+                WindowStyle = WindowStyle.None,
+                Width = 800,
+                Height = 300,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                DataContext = vm,
+                Content = new AddEmployeedForm()
+            };
+
+            window.Owner = parentWindow;
+            window.ShowDialog();
+            dgEmployees.ItemsSource = vm.Students;
+        }
+
+        private void imgDeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var id = (dgEmployees.SelectedItem as Employee).EmpID;
+            var vm = ((this.DataContext) as EmployeeViewModel);
+            vm.Deletestudent(id, this);
+            dgEmployees.ItemsSource = vm.Students;
+        }
+
+        private void btnAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = new Window
+            {
+                WindowStyle = WindowStyle.None,
+                Width = 800,
+                Height = 300,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                DataContext = new EmployeeViewModel(),
+                Content = new AddEmployeedForm()
+            };
+            window.Owner = this;
+            window.ShowDialog();
+
+            var vm = ((this.DataContext) as EmployeeViewModel);
+            dgEmployees.ItemsSource = vm.AddStudentsRecords();
+        }
+        public void StudentSearchFiler(object sender, EventArgs e)
+        {
+            var text = sender as string;
+
+            ICollectionView cv = CollectionViewSource.GetDefaultView(dgEmployees.ItemsSource);
+            cv.Filter = o =>
+            {
+                Employee obj = o as Employee;
+                return (obj.Name.ToLower().Contains(text.ToLower().Trim()) || obj.ContactNumber.ToLower().Contains(text.ToLower().Trim()));
+            };
+        }
+    }
+}
